6e48a1389354345319484ce2b694524bf2648e6b
Thomas Levine <occurrence@thomaslevine.com>
2013-08-31 17:34:22 +0000

    separate script

---
+++ script.js
@@ -0,0 +1,55 @@
+// Map across video elements
+function map(func) {
+  var videos = document.querySelector('#videos video')
+  var result = []
+  for (var i = 0; i < videos.length; i++) {
+    result.push(func(videos[i]))
+  }
+  return result
+}
+
+function play() {
+  map(function(v) { v.play() })
+}
+
+function pause() {
+  map(function(v) { v.pause() })
+}
+
+window.onload = function() {
+  // Seek to the appropriate places
+  var offsets = {
+    'back': 79.520136,
+    'above':   6.999342
+  }
+  var range = [19, (3746.000512 - offsets.back)]
+
+  // Video elements
+  var back  = document.querySelector('#videos .back')
+  var above = document.querySelector('#videos .above')
+  var seek  = document.querySelector('#seek')
+
+  // Synchronize videos
+  back.volume  = 0
+  above.volume = 1
+  back.currentTime  = offsets.back + range[0]
+  above.currentTime = offsets.back + range[0]
+
+  // Seek bar
+  seek.min = range[0]
+  seek.max = range[1]
+  seek.value = range[0]
+
+  seek.addEventListener('mouseup', seekFunc)
+  seek.addEventListener('keyup', seekFunc)
+  above.addEventListener('timeupdate', function() {
+    seek.value = above.currentTime
+  })
+  
+  function seekFunc() {
+    pause()
+    above.currentTime = seek.value
+    back.currentTime  = seek.value
+    play()
+  }
+}
