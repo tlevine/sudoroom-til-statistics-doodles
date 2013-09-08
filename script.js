aba16ee423545de2d889700efc764e5b0c0cd750
Thomas Levine <occurrence@thomaslevine.com>
2013-09-08 17:15:28 +0000

    st

--- script.js
+++ script.js
@@ -17,10 +17,10 @@ function pause() {
 window.onload = function() {
   // Seek to the appropriate places
   var offsets = {
-    'back':  79.520136,
-    'above':  6.999342
+    'back':  79.520136 + 19,
+    'above':  6.999342 + 19
   }
-  var range = [19, (3746.000512 - offsets.back)]
+  var length = 3746.000512 - offsets.back + 19
 
   // Video elements
   var back  = document.querySelector('#videos .back')
@@ -30,26 +30,22 @@ window.onload = function() {
   // Synchronize videos
   back.volume  = 0
   above.volume = 1
-  back.currentTime  = offsets.back  + range[0]
-  above.currentTime = offsets.above + range[0]
+  back.currentTime  = offsets.back
+  above.currentTime = offsets.above
 
   // Seek bar
-  seek.min = range[0]
-  seek.max = range[1]
-  seek.value = range[0]
-
   seek.addEventListener('blur', pause)
   seek.addEventListener('mousedown', pause)
   seek.addEventListener('mouseup', seekFunc)
   seek.addEventListener('keyup', seekFunc)
   above.addEventListener('timeupdate', function() {
     if (above.playing) {
-      seek.value = above.currentTime
+      seek.value = (above.currentTime - offsets.above) / length
     }
   })
   
   function seekFunc() {
-    above.currentTime = seek.value
-    back.currentTime  = seek.value < back.duration ? seek.value : back.duration
+    above.currentTime = length * seek.value + offsets.above
+    back.currentTime  = seek.value < ((back.duration - offsets.back)/ above.duration) ? length * seek.value + offsets.back: back.duration
   }
 }
